80afb5e07c55f0c200efec2c417f1d7d4a334b81
Roger Pate <rogerpate@gmail.com>
2024-11-18 01:58:05 -0500

    update and simplify

--- dataklasses.py
+++ dataklasses.py
@@ -14,130 +14,126 @@
 # the source code.  There is no warranty.  Try to use the code for the
 # greater good.
 
-__all__ = ["dataklass", "unfreeze"]
+__all__ = ["dataklass"]
 
 from functools import lru_cache, reduce, partial, wraps
 
 
 def codegen(func):
     @lru_cache
-    def make_func_code(numfields):
-        names = [f"_{n}" for n in range(numfields)]
-        exec(func(names), globals(), d := {})
+    def make_func_code(fields):
+        exec(func(fields), globals(), d := {})
         return d.popitem()[1]
 
     return make_func_code
 
 
-def patch_args_and_attributes(func, fields, start=0):
+def patch_attributes(func, fields):
+    co_names = tuple(fields.get(x, x) for x in func.__code__.co_names)
+    return type(func)(func.__code__.replace(co_names=co_names), func.__globals__)
+
+
+def patch_args_and_attributes(func, fields):
+    co_names = tuple(fields.get(x, x) for x in func.__code__.co_names)
+    co_varnames = tuple(fields.get(x, x) for x in func.__code__.co_varnames)
+    return type(func)(
+        func.__code__.replace(co_names=co_names, co_varnames=co_varnames),
+        func.__globals__,
+    )
+
+
+def patch__new__(func, fields):
+    co_names = tuple(fields.get(x, x) for x in func.__code__.co_names)
+    co_varnames = tuple(fields.get(x, x) for x in func.__code__.co_varnames)
+    co_consts = tuple(fields.get(x, x) for x in func.__code__.co_consts)
     return type(func)(
         func.__code__.replace(
-            co_names=(*func.__code__.co_names[:start], *fields),
-            co_varnames=("self", *fields),
+            co_names=co_names, co_varnames=co_varnames, co_consts=co_consts
         ),
         func.__globals__,
     )
 
 
-def patch_attributes(func, fields, start=0):
-    return type(func)(
-        func.__code__.replace(co_names=(*func.__code__.co_names[:start], *fields)),
-        func.__globals__,
-    )
-
-
-def all_hints(cls):
-    return reduce(lambda x, y: getattr(y, "__annotations__", {}) | x, cls.__mro__, {})
+def get_fields(cls):
+    fields = reduce(lambda x, y: getattr(y, "__annotations__", {}) | x, cls.__mro__, {})
+    fields = {f"_{n}": x for n, x in enumerate(fields, start=1)}
+    return fields
 
 
 @codegen
 def make__init__(fields):
     code = "def __init__(self, " + ",".join(fields) + "):\n"
-    code += "".join(f" self.{name} = {name}\n" for name in fields)
+    code += "".join(f"    self.{name} = {name}\n" for name in fields)
+    return code
+
+
+@codegen
+def make__new__(fields):
+    code = "def __new__(cls, " + ",".join(fields) + "):\n"
+    code += "    self = object.__new__(cls)\n"
+    code += "".join(
+        f"    object.__setattr__(self, {name!r}, {name})\n" for name in fields
+    )
+    code += "    return self\n"
     return code
 
 
 @codegen
 def make__repr__(fields):
-    return (
-        "def __repr__(self):\n"
-        " return f'{type(self).__name__}("
-        + ", ".join("{self." + name + "!r}" for name in fields)
-        + ")'\n"
-    )
+    code = "def __repr__(self):\n"
+    code += "    return f'{type(self).__name__}("
+    code += ", ".join(f"{{self.{name}!r}}" for name in fields)
+    code += ")'\n"
+    return code
 
 
 @codegen
 def make__eq__(fields):
-    selfvals = ",".join(f"self.{name}" for name in fields)
-    othervals = ",".join(f"other.{name}" for name in fields)
-    return (
-        "def __eq__(self, other):\n"
-        "  if self.__class__ is other.__class__:\n"
-        f"    return ({selfvals},) == ({othervals},)\n"
-        "  else:\n"
-        "    return NotImplemented\n"
-    )
+    selfvals = ", ".join(f"self.{name}" for name in fields)
+    othervals = ", ".join(f"other.{name}" for name in fields)
+    code = "def __eq__(self, other):\n"
+    code += "    if self.__class__ is other.__class__:\n"
+    code += f"        return ({selfvals},) == ({othervals},)\n"
+    code += "    else:\n"
+    code += "        return NotImplemented\n"
+    return code
 
 
 @codegen
 def make__iter__(fields):
-    return "def __iter__(self):\n" + "".join(
-        f"   yield self.{name}\n" for name in fields
-    )
+    code = "def __iter__(self):\n"
+    code += "".join(f"   yield self.{name}\n" for name in fields)
+    return code
 
 
 @codegen
 def make__hash__(fields):
-    self_tuple = "(" + ",".join(f"self.{name}" for name in fields) + ",)"
-    return f"def __hash__(self):\n    return hash({self_tuple})\n"
-
-
-class unfreeze:
-    def __init__(self, obj):
-        self.obj = obj
-
-    def __enter__(self):
-        object.__setattr__(self.obj, "_Frozen", False)
-        return self.obj
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.obj._Frozen = True
-
-
-def frozen_init(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwds):
-        with unfreeze(self):
-            func(self, *args, **kwds)
-
-    return wrapper
+    selfvals = ", ".join(f"self.{name}" for name in fields)
+    code = "def __hash__(self):\n"
+    code += f"    return hash(({selfvals},))\n"
+    return code
 
 
 def frozen_setattr(self, name, value):
-    if self._Frozen:
-        raise AttributeError(f"frozen dataklass cannot assign field {name}")
-    object.__setattr__(self, name, value)
+    raise TypeError(f"dataklass(frozen=True) cannot set attribute {name}")
 
 
 def frozen_delattr(self, name):
-    if self._Frozen:
-        raise AttributeError(f"frozen dataklass cannot delete field {name}")
-    object.__delattr__(self, name)
+    raise TypeError(f"dataklass(frozen=True) cannot delete attribute {name}")
 
 
-def dataklass(cls=None, *, frozen=False, slots=True, iter=False, hash=False):
+def dataklass(cls=None, *, frozen=False, iter=False, hash=False):
     if cls is None:
-        return partial(dataklass, frozen=frozen, slots=slots, iter=iter, hash=hash)
-    fields = all_hints(cls)
-    nfields = len(fields)
-    clsdict = vars(cls)
+        return partial(dataklass, frozen=frozen, iter=iter, hash=hash)
+    fields = get_fields(cls)
     if not fields:
         raise TypeError("dataklass must have at least one annotated field")
-    if "__init__" not in clsdict:
-        cls.__init__ = patch_args_and_attributes(make__init__(nfields), fields)
+    placeholders = tuple(fields)
+    clsdict = vars(cls)
+    if "__init__" not in clsdict and not frozen:
+        cls.__init__ = patch_args_and_attributes(make__init__(placeholders), fields)
     if frozen:
-        cls.__init__ = frozen_init(cls.__init__)
+        cls.__new__ = patch__new__(make__new__(placeholders), fields)
         if "__setattr__" in clsdict or "__delattr__" in clsdict:
             raise TypeError(
                 "dataklass(frozen=True) cannot use __setattr__ or __delattr__"
@@ -145,18 +141,16 @@ def dataklass(cls=None, *, frozen=False, slots=True, iter=False, hash=False):
         cls.__setattr__ = frozen_setattr
         cls.__delattr__ = frozen_delattr
     if "__repr__" not in clsdict:
-        cls.__repr__ = patch_attributes(make__repr__(nfields), fields, 2)
+        cls.__repr__ = patch_attributes(make__repr__(placeholders), fields)
     if "__eq__" not in clsdict:
-        cls.__eq__ = patch_attributes(make__eq__(nfields), fields, 1)
+        cls.__eq__ = patch_attributes(make__eq__(placeholders), fields)
     if iter:
         if "__iter__" in clsdict:
             raise TypeError("dataklass(iter=True) hides cls.__iter__")
-        cls.__iter__ = patch_attributes(make__iter__(nfields), fields)
+        cls.__iter__ = patch_attributes(make__iter__(placeholders), fields)
     if hash:
         if "__hash__" in clsdict:
             raise TypeError("dataklass(hash=True) hides cls.__hash__")
-        cls.__hash__ = patch_attributes(make__hash__(nfields), fields, 1)
-    cls.__match_args__ = tuple(fields)
-    if slots and "__slots__" not in clsdict:
-        cls.__slots__ = cls.__match_args__
+        cls.__hash__ = patch_attributes(make__hash__(placeholders), fields)
+    cls.__match_args__ = tuple(fields.values())
     return cls
